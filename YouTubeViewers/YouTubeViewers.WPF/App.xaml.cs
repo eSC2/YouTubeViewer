8f901dc77da5a61aba58287c194886b4160ae40c
eSC2 <quantm00.chan@gmail.com>
2022-04-24 00:18:35 +0800

    Setting up DB

--- YouTubeViewers/YouTubeViewers.WPF/App.xaml.cs
+++ YouTubeViewers/YouTubeViewers.WPF/App.xaml.cs
@@ -1,10 +1,16 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using YouTubeViewers.Domain.Commands;
+using YouTubeViewers.Domain.Queries;
+using YouTubeViewers.EntityFramework;
+using YouTubeViewers.EntityFramework.Commands;
+using YouTubeViewers.EntityFramework.Queries;
 using YouTubeViewers.WPF.Stores;
 using YouTubeViewers.WPF.ViewModels;
 
@@ -18,11 +24,26 @@ namespace YouTubeViewers.WPF
         private readonly ModalNavigationStore _modalNavigationStore;
         private readonly YouTubeViewersStore _youTubeViewersStore;
         private readonly SelectedYouTubeViewerStore _selectedYouTubeViewerStore;
+        private readonly YouTubeViewersDbContextFactory _youTubeViewersDbContextFactory;
+
+        private readonly IGetAllYouTubeViewersQuery _getAllYouTubeViewerCommand;
+        private readonly ICreateYouTubeViewerCommand _createYouTubeViewerCommand;
+        private readonly IUpdateYouTubeViewerCommand _updateYouTubeViewerCommand;
+        private readonly IDeleteYouTubeViewerCommand _deleteYouTubeViewerCommand;
 
         public App()
         {
+            string connectionString = "Data Source=YouTubeViewers.db";
+
+            _youTubeViewersDbContextFactory = new YouTubeViewersDbContextFactory(
+                new DbContextOptionsBuilder().UseSqlite(connectionString).Options);
+
             _modalNavigationStore = new ModalNavigationStore();
-            _youTubeViewersStore = new YouTubeViewersStore();
+            _getAllYouTubeViewerCommand = new GetAllYouTubeViewersQuery(_youTubeViewersDbContextFactory);
+            _createYouTubeViewerCommand = new CreateYouTubeViewerCommand(_youTubeViewersDbContextFactory);
+            _updateYouTubeViewerCommand = new UpdateYouTubeViewerCommand(_youTubeViewersDbContextFactory);
+            _deleteYouTubeViewerCommand = new DeleteYouTubeViewerCommand(_youTubeViewersDbContextFactory);
+            _youTubeViewersStore = new YouTubeViewersStore(_getAllYouTubeViewerCommand, _createYouTubeViewerCommand, _updateYouTubeViewerCommand, _deleteYouTubeViewerCommand);
             _selectedYouTubeViewerStore = new SelectedYouTubeViewerStore(_youTubeViewersStore);
         }
         protected override void OnStartup(StartupEventArgs e)
